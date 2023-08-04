c6f9f7704aa6fbc23272150eddfe9e297b236d8e
Hyungjun Lim <Hotfirebulldog@gmail.com>
2023-08-04 16:19:33 +0900

    feat.Star_08

--- src/DAY_001/STAR/Star_08.java
+++ src/DAY_001/STAR/Star_08.java
@@ -4,7 +4,7 @@ public class Star_08 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 9; j++) {
-                if (i + j >= 4 || (j - i <= 4 && j >= 5)) {
+                if ((i + j >= 4 && j <= 4) || (j - i <= 4 && j >= 5)) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
