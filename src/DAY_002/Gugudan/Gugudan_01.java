18a6d1930e359510435a094dd108f7870f69b2e8
Hyungjun Lim <Hotfirebulldog@gmail.com>
2023-08-06 17:05:35 +0900

    DAY_002.Gugudan finish

---
+++ src/DAY_002/Gugudan/Gugudan_01.java
@@ -0,0 +1,31 @@
+package DAY_002.Gugudan;
+
+public class Gugudan_01 {
+    public static void main(String[] args) {
+        System.out.println("1 X 1 = 1");
+        System.out.println("1 X 2 = 2");
+        System.out.println("1 X 3 = 3");
+        System.out.println("1 X 4 = 4");
+        System.out.println("1 X 5 = 5");
+        System.out.println("1 X 6 = 6");
+        System.out.println("1 X 6 = 1");
+        System.out.println("1 X 8 = 8");
+        System.out.println("1 X 8 = 8");
+        System.out.println("1 X 9 = 9");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("2 X " + i + " = " + 2 * i);
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("3 X " + i + " = " + 3 * i);
+        }
+
+        for (int j = 1; j <= 9; j++) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.println(j + " X " + i + " = " + j * i);
+            }
+        }
+
+    }
+}
---
+++ src/DAY_002/Gugudan/Gugudan_05.java
@@ -0,0 +1,21 @@
+package DAY_002.Gugudan;
+
+public class Gugudan_05 {
+    public static void main(String[] args) {
+        int[] result = new int[9];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = 2 * (i + 1);
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+
+        for (int j = 2; j < 10; j++) {
+            for (int i = 0; i < result.length; i++) {
+                result[i] = j * (i + 1);
+                System.out.println(result[i]);
+            }
+        }
+    }
+}
