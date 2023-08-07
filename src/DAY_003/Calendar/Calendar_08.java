06c79410ec2eec2cc87412612803b32b0c343a9d
Hyungjun Lim <Hotfirebulldog@gmail.com>
2023-08-07 18:34:17 +0900

    DAY_004.Array complete

--- src/DAY_003/Calendar/Calendar_08.java
+++ src/DAY_003/Calendar/Calendar_08.java
@@ -2,20 +2,33 @@
 //
 //import java.util.Scanner;
 //
+///**
+// * 1. 월을 입력하면 해당월의 달력을 출력한다.
+// * 2. 달력은 모양은 1단계에서 작성한 모양으로 만든다.
+// * 3. 1일은 일요일로 정해도 무방하다.
+// * 4. -1을 입력받기 전까지 반복 입력받는다.
+// * 5. 프롬프트를 출력한다.
+// */
 //public class Calendar_08 {
-//    public static void main(String[] args) {
-//        Calendar_05 calendar = new Calendar_05();
+//
+//    public void getPrompt() {
+//        System.out.println("월을 입력하세요.");
+//        System.out.print(">");
 //        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            System.out.println("월을 입력하세요.");
-//            System.out.print(">");
-//            int month = scanner.nextInt();
-//            if (month < 1 && month > 12) {
-//                break;
-//            }
-//            System.out.println(month + "월은 " + calendar.getMaxDaysOfMonth(month) + "일까지 있습니다.");
-//        }
-//        System.out.println("Hava a nice day!");
+//        int month = scanner.nextInt();
+//        printSampleCalendar(year, month);
+//
 //    }
+//    public static void main(String[] args) {
+//    }
+//
+//
+//    public int printSampleCalendar(int year, int month) {
+//        System.out.println("일  월  화 수  목 금  토");
+//        System.out.println("--------------------");
+//        System.out.println(" 1  2  3  4  5  6  7");
+//        System.out.println(" 8  9 10 11 12 13 14");
+//        System.out.println("15 16 17 18 19 20 21");
+//        System.out.println("22 23 24 25 26 27 28");
 //    }
 //}
