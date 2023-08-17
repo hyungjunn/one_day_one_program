788acf1d78c9105ee440c550160adacd2e7df0a9
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-17 19:54:38 +0900

    create DAY_014.CardGame

---
+++ src/DAY_014/CardGame/Card.java
@@ -0,0 +1,13 @@
+package DAY_014.CardGame;
+
+class Card {
+    String kind;
+    int num;
+
+    Card(){}
+
+    Card(String kind, int num){
+        this.kind = kind;
+        this.num = num;
+    }
+}
