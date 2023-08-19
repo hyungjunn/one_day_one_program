87c28b59f9c20ddb7d29850ba7299b6b3aa11517
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-19 22:21:23 +0900

    create PolyArgumentTest ver.07

--- src/DAY_015/PolyArgumentTest/Buyer.java
+++ src/DAY_015/PolyArgumentTest/Buyer.java
@@ -28,9 +28,24 @@ class Buyer {
         System.out.println(p + "을/를 구입하셨습니다.");
     }
 
+    void refund(Product p) {
+        if (item.remove(p)) {
+            money += p.price;
+            bonusPoint -= p.bonusPoint;
+            System.out.println(p + "을/를 반품하셨습니다.");
+        } else {
+            System.out.println("구입하신 제품 중 해당 제품이 없습니다.");
+        }
+    }
+
     void summary() {
         int sum = 0;
         String itemList = "";
+
+        if (item.isEmpty()) {
+            System.out.println("구입하신 제품이 없습니다.");
+            return;
+        }
 
         for (int i = 0; i < item.size(); i++) {
             Product p = (Product) item.get(i);
--- src/DAY_015/PolyArgumentTest/PolyArgumentTest.java
+++ src/DAY_015/PolyArgumentTest/PolyArgumentTest.java
@@ -3,14 +3,19 @@ package DAY_015.PolyArgumentTest;
 class PolyArgumentTest {
     public static void main(String[] args) {
         Buyer b = new Buyer();
+        Tv tv = new Tv();
+        Computer com = new Computer();
+        Audio audio = new Audio();
 
-        b.buy(new Tv());
-        b.buy(new Computer());
-        b.buy(new Audio());
+        b.buy(tv);
+        b.buy(com);
+        b.buy(audio);
 
         System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
         System.out.println("현재 보너스점수는 " + b.bonusPoint + "점입니다.");
 
+        b.refund(com);
         b.summary();
+
     }
 }
