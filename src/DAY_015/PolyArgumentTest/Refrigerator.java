06ffc33582cf684f566a8a7bec42359e690b007e
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-20 22:17:58 +0900

    create Refrigerator.class

---
+++ src/DAY_015/PolyArgumentTest/Refrigerator.java
@@ -0,0 +1,11 @@
+package DAY_015.PolyArgumentTest;
+
+class Refrigerator extends Product {
+    Refrigerator(){
+        super(500);
+    }
+
+    public String toString() {
+        return "Refrigerator";
+    }
+}
