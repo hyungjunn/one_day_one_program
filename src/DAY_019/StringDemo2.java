9058dd02cb242f88f29dca76c35f673513abf7b3
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-22 17:09:06 +0900

    create StringDemo2

---
+++ src/DAY_019/StringDemo2.java
@@ -0,0 +1,12 @@
+package DAY_019;
+
+class StringDemo2 {
+    public static void main(String[] args) {
+
+        String string1 = new String("Hello");
+        String string2 = "Hello";
+
+        System.out.println(string1.equals(string2));
+        System.out.println(string2.equals(string1));
+    }
+}
