d37b4353fb2ff877e11de2422c5d73cfd180587e
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-22 17:14:55 +0900

    create StringDemo4

---
+++ src/DAY_019/StringDemo4.java
@@ -0,0 +1,14 @@
+package DAY_019;
+
+class StringDemo4 {
+    public static void main(String[] args) {
+
+        String string1 = new String("Hello");
+
+        if (string1 == "Hello") {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
