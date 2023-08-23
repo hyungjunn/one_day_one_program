00182408ae23027dff0b678277d91be10ed1d047
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-23 14:29:06 +0900

    modify CardGame

--- src/DAY_014/CardGame/CardGame.java
+++ src/DAY_014/CardGame/CardGame.java
@@ -4,48 +4,69 @@ class CardGame {
     static String rankCheck(Card[] card) {
         String rank = "";
 
+        int[] counter = new int[13]; // card[i].num가 조건에 일치하는지 알아내기 위한 카운트도구
+        int[] counter2 = new int[4]; // card[i].kind가 조건에 일치하는지 알아내기 위한 카운트도구
+
+        for (int i = 0; i < 4; i++) {
+            if (card[i].kind == card[i + 1].kind) {
+                counter2[0]++;
+            }
+        }
+
         // 8. "STRAIGHT FLUSH" 같은 무늬면서 연속 숫자
-        if (card[0].kind == card[1].kind
-                && card[1].kind == card[2].kind
-                && card[2].kind == card[3].kind
-                && card[3].kind == card[4].kind
-                && card[0].num + 1 == card[1].num
-                && card[1].num + 1 == card[2].num
-                && card[2].num + 1 == card[3].num
-                && card[3].num + 1 == card[4].num)
-        { return "STRAIGHT FLUSH"; }
+        for (int i = 0; i < 4; i++) {
+            if (card[i].num + 1 == card[i + 1].num && counter2[0] == 4) {
+                return "STRAIGHT FLUSH";
+            }
+        }
 
         // 7. "FLUSH" 같은 무늬 5장
-        if (card[0].kind == card[1].kind
-                && card[1].kind == card[2].kind
-                && card[2].kind == card[3].kind
-                && card[3].kind == card[4].kind)
-        { return "FLUSH"; }
-
-
-        int[] counter = new int[13]; // 카드 몇개가 조건에 일치하는지 알아내기 위한 카운트도구
+        for (int i = 0; i < 4; i++) {
+            if (counter2[0] == 4) {
+                return "FLUSH";
+            }
+        }
 
         for (int i = 0; i < card.length; i++) {
             counter[card[i].num]++;
         }
 
-        for (int i = 0; i < counter.length; i++) {
-
-            if (counter[i] == 1 && counter[i + 1] == 1 && counter[i + 2] == 1 && counter[i + 3] == 1 && counter[i + 4] == 1) { return "STRAIGHT"; }
-            // 6. "STRAIGHT" 연속 숫자 5장
-
-            if (counter[i] == 4) { return "FOUR CARD"; }// 5. "FOUR CARD" 같은 카드 4장
-
-            if (counter[i] == 3) { return "THREE CARD"; }// 4. "THREE CARD" 같은 카드 3장
-
-            if (counter[counter[i]] == 2) { return "2 PAIR"; } // 3. "2 PAIR" 같은 숫자 한쌍
-
-            if (counter[i] == 2) { return "1 PAIR"; }    // 2. "1 PAIR" 같은 숫자 한쌍
-
-            if (counter[i] == 1) { return "NO RANK"; }   // 1. "NO RANK"
-
+        for (int i = 0; i < counter.length; i++) {// 6. "STRAIGHT" 연속 숫자 5장
+            if (counter[i] == 1 && counter[i + 1] == 1 && counter[i + 2] == 1 && counter[i + 3] == 1 && counter[i + 4] == 1) {
+                return "STRAIGHT";
+            }
         }
 
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 4) {
+                return "FOUR CARD";
+            }// 5. "FOUR CARD" 같은 카드 4장
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 3) {
+                return "THREE CARD";
+            }// 4. "THREE CARD" 같은 카드 3장
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[counter[i]] == 2) {
+                return "2 PAIR";
+            } // 3. "2 PAIR" 같은 숫자 한쌍
+        }
+
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 2) {
+                return "1 PAIR";
+            }    // 2. "1 PAIR" 같은 숫자 한쌍
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 1) {
+                return "NO RANK";
+            }   // 1. "NO RANK"
+        }
         return rank;
     }
 
