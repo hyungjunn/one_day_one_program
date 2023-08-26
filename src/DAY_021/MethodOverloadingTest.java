cf404fdb3fce25201125d4da7415fc51b410d133
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-26 21:50:58 +0900

    add println(float f)

--- src/DAY_021/MethodOverloadingTest.java
+++ src/DAY_021/MethodOverloadingTest.java
@@ -5,9 +5,19 @@ public class MethodOverloadingTest {
         System.out.println(b);
     }
 
+    public void println(int i) {
+        System.out.println(i);
+    }
+
+    public void println(float f) {
+        System.out.println(f);
+    }
+
 
     public static void main(String[] args) {
         MethodOverloadingTest methodOverloadingTest = new MethodOverloadingTest();
         methodOverloadingTest.println(true);
+        methodOverloadingTest.println(100);
+        methodOverloadingTest.println(3.14f);
     }
 }
