7e13e9497d4a43b163d5eeed12cc2b10fd26bab7
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-28 13:40:34 +0900

    add another case

--- src/DAY_022/MathRoundTest.java
+++ src/DAY_022/MathRoundTest.java
@@ -7,7 +7,18 @@ public class MathRoundTest {
         // int result2 = Math.round(3.14d); 큰 범위 -> 작은 범위 : 형변환 생략 X
         int result2 = (int) (Math.round(3.14443d));
 
+        int result3 = Math.round(-3.14f);
+
+        long result4 = Math.round(-1.5);
+        long result5 = Math.round(-0.5);
+        int result6 = (int)(Math.round(-1.5));
+        // 음수는 반올림이 안된다.
+
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
     }
 }
