f58c382359b784cc2d942d15596956db7d2cffe2
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-08-28 13:08:37 +0900

    add other case

--- src/DAY_022/MathRoundTest.java
+++ src/DAY_022/MathRoundTest.java
@@ -2,9 +2,10 @@ package DAY_022;
 
 public class MathRoundTest {
     public static void main(String[] args) {
-        long result1 = Math.round(3.14f);
+        // Math.round() 는 소수점 첫째자리에서 반올림
+        long result1 = Math.round(3.94f);
         // int result2 = Math.round(3.14d); 큰 범위 -> 작은 범위 : 형변환 생략 X
-        int result2 = (int) (Math.round(3.14d));
+        int result2 = (int) (Math.round(3.14443d));
 
         System.out.println(result1);
         System.out.println(result2);
