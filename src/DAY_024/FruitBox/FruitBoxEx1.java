861881719d82c339088ca93c8a693d555c0d6509
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-01 08:12:48 +0900

    modified FruitBox

--- src/DAY_024/FruitBox/FruitBoxEx1.java
+++ src/DAY_024/FruitBox/FruitBoxEx1.java
@@ -5,17 +5,24 @@ class FruitBoxEx1 {
         Box<Fruit> fruitBox = new Box<Fruit>();
         Box<Apple> appleBox = new Box<Apple>();
         Box<Toy> toyBox = new Box<Toy>();
-        // Box<Grape> grapeBox = new Box<Apple>(); // 에러. 타입 불일치
+        Box<Grape> grapeBox = new Box<Grape>();
+        // Box<Grape> grapeBox1 = new Box<Apple>();
 
         fruitBox.add(new Fruit());
         fruitBox.add(new Apple()); // OK. void add(Fruit item)
 
         appleBox.add(new Apple());
         appleBox.add(new Apple());
-        // appleBox.add(new Toy()); // 에러. Box<Toy>에는 Apple을 담을 수 없음.
+        // appleBox.add(new Toy()); // 에러. Box<Apple>에는 Apple만을 담을 수 있음
+        toyBox.add(new Toy());
 
         toyBox.add(new Toy());
-        // toyBox.add(new Apple()); // 에러. Box<Toy>에는 Apple을 담을 수 없음.
+        toyBox.add(new Toy());
+        toyBox.add(new Toy());
+
+        for (int i = 0; i < 5; i++) {
+            toyBox.add(new Toy());
+        }
 
         System.out.println(fruitBox);
         System.out.println(appleBox);
