bb14f5c5b14732ea0f62bb567eac6d77fed9ba41
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-03 23:37:25 +0900

    complete Fibonacci

--- src/com/day_026/Fibonacci.java
+++ src/com/day_026/Fibonacci.java
@@ -9,30 +9,10 @@ class Fibonacci {
         System.out.print(num1 + ", " + num2);
 
         for (int i = 0; i < 8; i++) {
-//            num3 = num1 + num2;
-//            System.out.print(", " + num3);
-//            num4 = num3 + num2 = (num1 + num2) + num2
-//            System.out.print(", " + num4);
-//            num5 = num4 + num3 = (num1 + num2 + num2) + (num1 + num2)
-//            System.out.print(", " + num5);
-//            num6 = num5 + num4 = (num1 + num2 + num2) + (num1 + num2) + (num1 + num2 + num2)
-//            System.out.print(", " + num6);
-//            num7 = num6 + num5;
-//            System.out.print(", " + num7);
-//            num8 = num7 + num6;
-//            System.out.print(", " + num8);
-//            num9 = num8 + num7;
-//            System.out.print(", " + num9);
-//            num10 = num9 + num8;
-//            System.out.print(", " + num10);
             num3 = num1 + num2;
             System.out.print(", " + num3);
-            num2 += num3;
+            num1 = num2;
+            num2 = num3;
         }
     }
 }
-// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-// num3 = num1 + num2
-// num4 = num1 + num2 + num2
-// num5 = num1 + num2 + num2 + num3
-// num6 = num1 + num2 + num2 + num3 + num4
