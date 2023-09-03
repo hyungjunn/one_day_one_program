f00b2712eb6d21922ab9005c513c664ebe9a717c
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-03 15:43:29 +0900

    create ScanenrTest

---
+++ src/DAY_026/ScannerTest.java
@@ -0,0 +1,10 @@
+package DAY_026;
+
+import java.util.Scanner;
+
+class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
