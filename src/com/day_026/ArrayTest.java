78109026a2d3dffa807234b5e7116190bdea45c4
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-04 11:50:44 +0900

    create sortArr

--- src/com/day_026/ArrayTest.java
+++ src/com/day_026/ArrayTest.java
@@ -5,6 +5,21 @@ class ArrayTest {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6};
 
         printArr(arr);
+        sortArr(arr);
+        printArr(arr);
+        System.out.println(sumArr(arr));
+    }
+
+    static void sortArr(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
     }
 
     static void printArr(int[] arr) {
@@ -16,5 +31,11 @@ class ArrayTest {
         System.out.println("]");
     }
 
-
+    static int sumArr(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 }
