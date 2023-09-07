342dc88075a801054077feaaa33bcae77f1db45a
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-07 23:16:01 +0900

    add car2

--- src/com/day_029/CarTest.java
+++ src/com/day_029/CarTest.java
@@ -2,8 +2,13 @@ package com.day_029;
 
 public class CarTest {
     public static void main(String[] args) {
-        Car car = new Car();
-        System.out.println(car);
-        car.run();
+        Car car1 = new Car();
+        System.out.println(car1);
+        car1.run();
+        car1.printName();
+        System.out.println("---------------------");
+
+        Car car2 = new Car("TESLA");
+        car2.printName();
     }
 }
