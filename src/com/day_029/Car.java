613a2f04231247a5b8616c862d5eb3144f9e121f
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-07 22:59:59 +0900

    create Car.class

---
+++ src/com/day_029/Car.java
@@ -0,0 +1,14 @@
+package com.day_029;
+
+public class Car {
+
+    public Car() {
+        System.out.println("자동차가 한 대 생성됩니다.");
+    }
+
+    public void run() {
+        System.out.println("전륜구동으로 달리다.");
+    }
+
+
+}
