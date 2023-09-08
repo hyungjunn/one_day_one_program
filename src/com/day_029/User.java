34c55442d621c1cbf464fa182f137a5e82c83b37
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-08 09:47:38 +0900

    create UserTest

--- src/com/day_029/User.java
+++ src/com/day_029/User.java
@@ -3,5 +3,23 @@ package com.day_029;
 class User {
     private String email;
     private String password;
-    private String name ;
+    private String name;
+
+    // 생성자를 하나라도 만들면 자동으로 안만들어진다.
+    public User(String name, String email) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ src/com/day_029/UserTest.java
@@ -0,0 +1,10 @@
+package com.day_029;
+
+class UserTest {
+    public static void main(String[] args) {
+        User user = new User("임형준", "hotfirebulldog@gmail.com");
+
+        System.out.println(user.getName());
+        System.out.println(user.getEmail());
+    }
+}
