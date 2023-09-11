6c453dbb00c11c334fcd7b304c346778f06c9269
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-12 00:13:46 +0900

    create LottoMachineMain

--- src/com/hyungjunn/example/day_032/util/LottoMachineMain.java
+++ src/com/hyungjunn/example/day_032/util/LottoMachineMain.java
@@ -2,8 +2,18 @@ package com.hyungjunn.example.day_032.util;
 
 public class LottoMachineMain {
     public static void main(String[] args) {
-
+        Ball[] balls = new Ball[45];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = new Ball(i + 1);
+        }
         // LottoMachine인스턴스가 생성된다.
         LottoMachine lottoMachine = new LottoMachineImpl();
+        lottoMachine.setBalls(balls);
+        lottoMachine.mix();
+        Ball[] result = lottoMachine.getBalls();
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i].getNumber());
+        }
     }
 }
