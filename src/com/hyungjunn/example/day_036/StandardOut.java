2db7ecbb09e54e0ae793ca3bf996955ecc5af5f7
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-21 22:30:09 +0900

    create StandardOutTest

---
+++ src/com/hyungjunn/example/day_036/StandardOut.java
@@ -0,0 +1,23 @@
+package com.hyungjunn.example.day_036;
+
+public class StandardOut {
+    public void println(){
+        System.out.println();
+    }
+
+    public void println(boolean b) {
+        System.out.println(b);
+    }
+
+    public void println(int i) {
+        System.out.println(i);
+    }
+
+    public void println(double d) {
+        System.out.println(d);
+    }
+
+    public void println(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/com/hyungjunn/example/day_036/StandardOutTest.java
@@ -0,0 +1,11 @@
+package com.hyungjunn.example.day_036;
+
+public class StandardOutTest {
+    public static void main(String[] args) {
+        StandardOut sdo = new StandardOut();
+        sdo.println(5);
+        sdo.println(3.14);
+        sdo.println("Hello");
+        sdo.println(true);
+    }
+}
